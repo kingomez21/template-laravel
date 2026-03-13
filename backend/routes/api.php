fd6da33ed6223c033ffc74721cea109cbe9213a0
kingomez21 <juansebastiangomezmezu@gmail.com>
2026-03-13 13:39:59 -0500

    adjustment login

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register'])
+    ->middleware('api');
+Route::post('/login',    [AuthController::class, 'login'])
+    ->middleware('api');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user',   fn (Request $request) => $request->user());
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
